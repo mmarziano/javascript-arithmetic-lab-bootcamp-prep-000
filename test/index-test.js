d20d3738eb9764c1253d3d709c4a2744daa218e8
Monica Marziano <mmarziano1@gmail.com>
2017-11-11 22:23:00 +0000

    automatically backed up by learn

--- test/index-test.js
+++ test/index-test.js
@@ -1,9 +1,8 @@
 var a, b
 
-beforeEach(function add(a,b) {
+beforeEach(function() {
   a = Math.floor(Math.random() * 1000);
   b = Math.floor(Math.random() * 1000);
-  return a + b;
 })
 
 it('add(a, b) adds two numbers and returns the result', function() {
